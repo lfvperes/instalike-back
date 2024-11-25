05de40b61e36691863342bd587cf1d0d9ea2bcb6
lfvperes <luisfvperes@gmail.com>
2024-11-25 17:08:56 -0300

    front end integration

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,19 @@
 import express from "express";
 import multer from "multer";
 import { listAllPosts, makeNewPost, imageUpload, updateNewPost } from "../controllers/postsController.js";
+import cors from "cors";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+};
 
 const upload = multer({dest: "./uploads"})
 
 const routes = (app) => {
     // Enable JSON parsing for incoming requests
     app.use(express.json());
+    app.use(cors(corsOptions));
 
     // Define a route handler for the '/posts' endpoint
     app.get("/posts", listAllPosts);
